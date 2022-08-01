0d5bac7e01d95d354d9c4bf2fb072c70c0f6553b
Angelica Orozco <angorozcar@gmail.com>
2022-08-01 16:11:58 -0300

    some scrolling effects

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
 const menu = document.getElementById("burger");
 const list = document.getElementById("navigation");
+const arrow = document.getElementById("arrow");
 
 menu.addEventListener("click", () => {
-    console.log("clic");
     list.classList.toggle("show");
+});
+
+window.addEventListener("scroll", () => {
+    console.log("inner", window.pageYOffset);
+    if (window.pageYOffset > 650) {
+        arrow.classList.add("arrowshow");
+    } else {
+        arrow.classList.remove("arrowshow");
+    }
 });
